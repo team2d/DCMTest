ef195607d23fb6b3c733915880b66174f8162d90
team2d <team2d@ya.ru>
2016-04-27 19:29:33 +0300

    commit test

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-//kjhkhjgk
+/*
+ The list of cards fields:
+- series (3 uppercase letters)
+- number (6 digits)
+- issue date (also includes time)
+- expiration date
+- status (active / inactive / expired)
+Search by the fields above.
+Cards list pagination (25 items per page)
+Card profile page with last card usage date, total purchases amount and card purchase history (please fill in the database with some mock data).
+Card activation and deactivation (on the fly using AJAX).
+Card deletion.
+Cards generator. Input values are: series, expiration period (values "1 month", "6 months", "one year") and number of cards to be generated.
+When card expiration date is passed the card gets a status "expired".
+All code is covered with PHP and JS unit tests.
+Pages are styled with CSS*/
 ?>
